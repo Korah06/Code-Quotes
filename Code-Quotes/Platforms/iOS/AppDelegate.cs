b1515e5b01922c9e3a50b027829ede7d8641fed1
Korah06 <Korah06@DESKTOP-ENTIKTF>
2023-03-15 10:31:10 +0100

    Agregar archivos de proyecto.

---
+++ Code-Quotes/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace Code_Quotes;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
